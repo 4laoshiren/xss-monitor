da330ce9c368b990d3fdd7ae8d473a69f9fc4dba
zhongzhong <2322916463@qq.com>
2026-06-26 14:16:18 +0800

    fix wrong argument name and add dependency. - smtp.auth.password ->smtp.auth.pass - add cors - format

--- config.js
+++ config.js
@@ -2,14 +2,14 @@
 // 自己给自己发邮件
 export default {
     port: 3000,
-    notifyTo: 'your mail',
+    notifyTo: "your mail",
     smtp: {
-        host: 'smtp server of your mail',
+        host: "smtp server of your mail",
         port: 465,
         secure: true,
         auth: {
-            user: 'your mail',
-            password: 'your mail password'
-        }
-    }
-}
+            user: "your mail",
+            pass: "your mail password",
+        },
+    },
+};
